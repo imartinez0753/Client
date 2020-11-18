aeba2ec5eeeb6a7a047e0c2d6f61a8ec6da19aee
Ian Martinez <imartinez0753@gmail.com>
2020-11-17 17:27:22 -0800

    Return the running sum of nums

--- chops.js
+++ chops.js
@@ -2,10 +2,18 @@
 // Output: [0,1]
 // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-var nums = [2,7,11,15] 
+// var nums = [2,7,11,15] 
 
-for (var i = 0; i < nums.length; i++) {
-    if (nums[i] + nums[i] === 9) {
-        console.log("ture")
-    }
-}
+// for (var i = 0; i < nums.length; i++) {
+//     if (nums[i] + nums[i] === 9) {
+//         console.log("true")
+//     }
+// }
+
+
+// console.log("hello World");
+
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
